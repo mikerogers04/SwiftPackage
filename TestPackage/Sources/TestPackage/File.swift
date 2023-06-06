daac023e7c964932b0583eb59dd97c05e28ecdc9
mikerogers04 <mikerogers04@hotmail.com>
2023-06-06 11:06:47 -0700

    Adds multiply function.

---
+++ TestPackage/Sources/TestPackage/File.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by LEARN Academy on 6/6/23.
+//
+
+import Foundation
+
+
+public func add() {
+    let num1: Int = 2
+    let num2: Int = 2
+    print(num1 + num2)
+}
+
+public func subtract() {
+    let num3: Int = 4
+    let num4: Int = 3
+    print(num3 - num4)
+}
+
+public func multiply() {
+    let num5: Int = 5
+    let num6: Int = 5
+    print(num5 * num6)
+}
