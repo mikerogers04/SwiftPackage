652f100719cc3b355d534e22aa1b4eef89f38b69
mikerogers04 <mikerogers04@hotmail.com>
2023-06-06 12:51:53 -0700

    Removes a file.

--- Sources/TestPackage/Math.swift
+++ Sources/TestPackage/Math.swift
@@ -36,26 +36,3 @@ public struct math {
     public init() {
     }
 }
-//public func add() {
-//    let num1: Int = 2
-//    let num2: Int = 2
-//    print(num1 + num2)
-//}
-//
-//public func subtract() {
-//    let num3: Int = 4
-//    let num4: Int = 3
-//    print(num3 - num4)
-//}
-//
-//public func multiply() {
-//    let num5: Int = 5
-//    let num6: Int = 5
-//    print(num5 * num6)
-//}
-//
-//public func divide() {
-//    let num7: Int = 6
-//    let num8: Int = 3
-//    print(num7 * num8)
-//}
