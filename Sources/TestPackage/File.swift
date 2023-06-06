ba018640df47c50d7d4da1c897db88f0f163dce7
mikerogers04 <mikerogers04@hotmail.com>
2023-06-06 09:31:50 -0700

    Initial Commit

---
+++ Sources/TestPackage/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by LEARN Academy on 6/6/23.
+//
+
+import Foundation
+
+
+public func add() {
+    let num1: Int = 2
+    let num2: Int = 2
+    print(num1 + num2)
+}
